162deef004d78902e9f3a958933fdcf1f4c724fa
Swati Priyambda <priyambda1307@gmail.com>
2026-06-23 12:42:49 +0530

    Add files via upload

--- MultiplyStrings.java
+++ MultiplyStrings.java
@@ -1,34 +1,53 @@
+package StringsProb;
+
 public class MultiplyStrings {
     public String multiply(String num1, String num2) {
-        int n = num1.length();
-        int m = num2.length();
+        /*
+         * For every pair of digits, multiply them, figure out which column (place
+         * value) the result belongs to, and dump it directly into that index in the
+         * result array. At the end, handle carries and build the string.
+         */
 
-        int[] result = new int[m+n];
-        for(int i = n-1 ; i >= 0 ; i-- ){
+        char[] digit1 = num1.toCharArray();
+        char[] digit2 = num2.toCharArray();
 
-            for(int j = m-1 ; j >= 0 ; j--){
+        //create result array of size n1+n2, filled with 0s
+        int n1 = num1.length();
+        int n2 = num2.length();
+        int[] result = new int[n1+n2];
 
-                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+        //Now for filling the result array
+        for(int i = n1-1 ; i >= 0 ; i--){
+            for(int j = n2-1 ; j >= 0 ; j--){
+                int product = (digit1[i]-'0')*(digit2[j]-'0');
 
-                int ones = mul%10;
-                int tens = mul/10;
+                int pos_digit = i+j+1;    // where product's unit digit goes
+                int pos_carry = i+j;      // where carry goes
 
-                result[i+j+1] += ones;
-                result[i+j]+=tens;
+                int sum = product + result[pos_digit];    // add what's already there
+
+                result[pos_digit] = sum%10;     //placing the unit digit
+                result[pos_carry] = sum/10;     //placing the carry 
             }
         }
 
-        //handle the two digit number in the array ie the carry from right to left
-        for(int k = m+n-1 ; k > 0 ; k--){
-            result[k-1]+=result[k]/10;
-            result[k]+=result[k]%10;
+        //Now making the string
+        StringBuilder sb = new StringBuilder();
+
+        int nonZero = -1;
+        for(int i = 0 ; i < result.length ; i++){
+            if (result[i] != 0) {
+                nonZero = i;
+                break;
+            }
         }
 
-        //Build the string
-        StringBuilder sb = new StringBuilder();
-        for(int digit : result){
-            if(sb.length() == 0 && digit == 0) continue; //skip the leading zeroes
-            sb.append(digit);
+        if (nonZero != -1) {
+            return "0";
+        }
+
+        for(int j = nonZero ; j < result.length ; j++){
+            sb.append(result[j]);
         }
 
         return sb.toString();
