08f736f5236982a34f94680ac04086f6e6809bcc
Swati Priyambda <priyambda1307@gmail.com>
2026-06-26 04:21:51 +0530

    Add files via upload

---
+++ SymmetricTree.java
@@ -0,0 +1,11 @@
+package TreesProb;
+
+public class SymmetricTree {
+     public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+
+        
+    }
+}
