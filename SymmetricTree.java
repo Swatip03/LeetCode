f1d7b69e286add85607e2d4e6117c6179297ca40
Swati Priyambda <priyambda1307@gmail.com>
2026-07-04 01:36:28 +0530

    Add files via upload

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -2,10 +2,27 @@ package TreesProb;
 
 public class SymmetricTree {
      public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
+        
+      //iMAgine a mirror is placed at the centre ie the root of the tree
+      //then ele of the left subtree should be equal to the right subtree
+      //Property of mirror : left becomes right and right becomes left
+           if (root == null) {
+            return false;
+           }
+
+           return findSymmetric(root.left, root.right);
+    }
+
+
+    public boolean findSymmetric(TreeNode left , TreeNode right){
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        if (left.val != right.val) {
             return false;
         }
 
-        
+        return findSymmetric(left.left, right.right) && findSymmetric(left.right, right.left);
     }
 }
