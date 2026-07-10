40882987e087ce5ec70b36ecee7a3d760c368fa2
Swati Priyambda <priyambda1307@gmail.com>
2026-07-10 14:14:49 +0530

    Add files via upload

---
+++ Triplets.java
@@ -0,0 +1,13 @@
+package TreesProb;
+
+public class Triplets {
+    TreeNode node;
+    int row;
+    int col;
+
+    Triplets(TreeNode node , int row , int col){
+        this.node = node;
+        this.row = row;
+        this.col = col;
+    }
+}
