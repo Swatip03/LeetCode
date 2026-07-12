1da33b2c9704769bfcd8f342e7d92ee526e3298d
Swati Priyambda <priyambda1307@gmail.com>
2026-07-12 18:54:09 +0530

    Add files via upload

--- KokoEatingBananas.java
+++ KokoEatingBananas.java
@@ -1,36 +1,36 @@
+package BinarySearchProb;
+
+import java.util.Arrays;
 
 public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int h) {
+          Arrays.sort(piles);
+          int low = 1;
+          int high = piles[piles.length-1];
+          int ans = 1;
 
-    public int canEatInHr(int[] piles , int k){
-        int n = piles.length;
-        int hr = 0;
-        for(int i = 0 ; i < n ; i++){
-            if (piles[i]%k != 0) {
-                hr+=piles[i]/k+1;
+          //Remember the monotonic relationship: as k increases, takenHr decreases (or stays same).
+          while (low <= high) {
+            int mid = low + (high-low)/2;
+
+            if (takenHr(piles, mid) <= h) {
+                ans = mid;
+                high = mid-1; //search in the left half for lower k value
             }else{
-                hr+=piles[i]/k;
+                low = mid+1;   //this mid is too slow → need a faster speed hence search in the right half
             }
-        }
-        return hr;
+          }
+
+          return ans;
     }
 
-    public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
-        int s = 1;         // the minimum availabe value of k
-        int e = piles[n-1];
-        int ans = e;
+    public int takenHr(int[] nums , int hourly){
+        int totalHr = 0;
 
-        while (s<=e) {
-            int mid = s + (e-s)/2;
-            int hours = canEatInHr(piles, mid);
-            if (hours <= h) {
-                ans = mid;       // The possible answer 
-                 e = mid-1;       //✅ try smaller speed
-            }else{
-                s = mid+1;       //Too slow , increase speed
-            }
+        for(int i = 0 ; i < nums.length ; i++){
+            totalHr += Math.ceilDiv(nums[i], hourly);
         }
-
-        return ans;
+        
+        return totalHr;
     }
 }
