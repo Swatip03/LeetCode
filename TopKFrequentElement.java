2017dd190fb15d6ffd3005fae73824cbbd6a82a8
Swati Priyambda <priyambda1307@gmail.com>
2026-07-17 01:37:02 +0530

    Add files via upload

--- TopKFrequentElement.java
+++ TopKFrequentElement.java
@@ -1,46 +1,39 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
+
+import java.util.*;
 public class TopKFrequentElement {
-    
     public int[] topKFrequent(int[] nums, int k) {
-
-        HashMap<Integer,Integer> hmap = new HashMap<>();
-
-        for(int i = 0 ; i < nums.length ; i++){
-
+         //First part take the elements of nums and push it into hashmap such that
+         //it creates a pair of (ele : freq)    
+         int n = nums.length;
+         HashMap<Integer,Integer> hmap = new HashMap<>();
+         for(int i = 0 ; i < n ; i++){
             if (hmap.containsKey(nums[i])) {
-
                 hmap.put(nums[i], hmap.get(nums[i])+1);
-                
-            }
-
-            else{
-
+            }else{
                 hmap.put(nums[i], 1);
-
             }
-
         }
 
-        
+        //Now putting these pairs in the priority queue & the pairs are sorted in the frequency wise order
+        //Min heap keeps the least frequent on top so it gets kicked out first when someone better comes along
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
 
 
-        
-    }
+        for(Map.Entry<Integer,Integer> entry : hmap.entrySet()){
+            pq.add(new int[]{entry.getKey(),entry.getValue()});
 
-    public static void main(String[] args) {
-        
-        TopKFrequentElement tf = new TopKFrequentElement();
+            if (pq.size() > k) {   //Remove the element when its size gets bigger than the k
+                pq.poll();
+            }
+        }
 
-        int[] nums = {1,1,1,2,2,3};
-        int k = 2;
-
-        tf.topKFrequent(nums, k);
-
-
-
-
+        //Now answer of the problem since we have to find the k frequent element
+        int[] ans = new int[k];
+        for(int i = 0; i < k ; i++){
+            ans[i] = pq.poll()[0];
+        }
+         
+        return ans;
     }
 }
