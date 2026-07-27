bccaaa7c7b09e212c779b8ef6c99fe5a29b4ae82
Swati Priyambda <priyambda1307@gmail.com>
2026-07-28 00:46:08 +0530

    Add files via upload

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -1,70 +1,48 @@
-import java.util.Arrays;
+package BinarySearchProb;
 
 public class MedianOfTwoSortedArrays {
-    
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        int m = nums1.length;
-        int n = nums2.length;
+        //As we only want to apply binary search on the shorter array which we are assuming to be nums1
+        if(n1  > n2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int[] nums = new int[m+n];
+        int low = 0;
+        int high = n1;
+        int left = (n1+n2+1)/2;  //we require this many elements on the left
+        int n = n1+n2;
 
-        // Fill the nums array
+        while (low <= high) {
+            int mid1 = low + (high-low)/2;
+            int mid2 = left-mid1;
 
-        for(int i = 0; i < nums.length ; i++){
+            int l1 = Integer.MIN_VALUE , l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE , r2 = Integer.MAX_VALUE;
 
-            if (i < m) {
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
 
-                nums[i] = nums1[i];
-                
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+            if (l1 <= r1 && l2 <= r2) {
+                if(n % 2 != 0){ //if odd
+                    return (double)Math.max(l1, l2);
+                }else{
+                    return (double)(Math.max(l1, l2) + Math.min(r1, r2))/2.0;
+                }
+            }else if (l1 > r2) {
+                high = mid1-1;
+            }else{
+                low = mid1+1;
             }
-            else{
 
-                nums[i] = nums2[i-m];
-
-            }
-
-        }
-
-        // Sort the array
-
-        Arrays.sort(nums);
-
-        // Find the median
-
-        double median = 0;
-
-        int l = nums.length;
-
-        if (l % 2 != 0) {
-
-           median = nums[l/2];
-            
-        }
-        else{
-
-            median = (nums[l/2] + nums[(l/2) - 1]) / 2.0;
-
-        }
-
-
-        return median;
-        
+         }
+     
+         return 0.0;
     }
 
-
-    public static void main(String[] args) {
-
-        MedianOfTwoSortedArrays mm = new MedianOfTwoSortedArrays();
-        
-        int[] nums1 = {1,2};
-        int[] nums2 = {3,4};
-
-        double median = mm.findMedianSortedArrays(nums1, nums2);
-
-
-        System.out.println(median);
-
-
-    }
 }
